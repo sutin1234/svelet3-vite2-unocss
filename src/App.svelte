6b7823b30ae1153d52e0ccab5798917e9e3062dc
Sutin <poly.dev2018@gmail.com>
2021-11-13 17:25:56 +0700

    added unocss

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<style>
+:root {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
+
+<main class="text-center">
+  <h1 class="text-blue-500">Hello Typescript!</h1>
+  <h1 class="text-green-500">Text color from UnoCSS</h1>
+
+  <p>
+    Visit
+    <a href="https://svelte.dev">svelte.dev</a>
+    to learn how to build Svelte apps.
+  </p>
+  <p>
+    Check out
+    <a href="https://github.com/sveltejs/kit#readme">SvelteKit</a>
+    for the officially supported framework, also powered by Vite!
+  </p>
+</main>
